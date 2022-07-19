a647a9945f9a25906bbed30936e85aa1cfb2d67c
Mohamed Hesham <106884579+mohos455@users.noreply.github.com>
2022-07-19 13:22:15 +0200

    Add Jop Urls

--- job/models.py
+++ job/models.py
@@ -15,8 +15,17 @@ class Job(models.Model):
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    Category = models.ForeignKey('category',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class category(models.Model):
+
+    name =models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
 
 
